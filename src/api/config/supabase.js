01b86f9eb0f43d63caa02fc789efc812d74c47b0
Riyukazee <mhrultimate@gmail.com>
2025-11-25 18:39:11 +0100

    Update supabase.js
    const { createClient } = require("@supabase/supabase-js");
    
    const supabase = createClient(
      process.env.VITE_SUPABASE_URL,
      process.env.VITE_SUPABASE_ANON_KEY
    );
    
    module.exports = { supabase };

--- src/api/config/supabase.js
+++ src/api/config/supabase.js
@@ -1,1 +1,9 @@
+const { createClient } = require("@supabase/supabase-js");
+
+const supabase = createClient(
+  process.env.VITE_SUPABASE_URL,
+  process.env.VITE_SUPABASE_ANON_KEY
+);
+
+module.exports = { supabase };
 
